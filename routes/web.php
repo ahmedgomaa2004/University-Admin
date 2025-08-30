925996c73d5899b747f0e396ef202867c4399faa
Ahmed <h73042020@gmail.com>
2025-08-30 03:04:55 +0300

    install brez

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoctorsController;
 use App\Http\Controllers\StudentsController;
 use App\Http\Controllers\CoursessController;
 use App\Http\Controllers\EmployeesController;
 use App\Http\Controllers\DepartmentsController;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
 /*
@@ -19,6 +20,23 @@ use Illuminate\Support\Facades\DB;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 
 
@@ -46,4 +64,3 @@ Route::resource("courses",CoursessController::class);
 Route::resource("employees",EmployeesController::class);
 
 Route::resource("departments",DepartmentsController::class);
-
